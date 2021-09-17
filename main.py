049ab7611baff8da1a55ca050c1e0c2e71fff684
MarcellusCodes <MarcellusCodes@gmail.com>
2021-09-17 20:37:20 +0200

    Added Staticfiles configuration, font family, css file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from utils.server import app
 from utils.template import templates
 from core.views.landing import landing
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
